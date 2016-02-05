a21319d6dd261aa1f0f6aa8568bc09a4cb881a8e
cristian-ungureanu <cristian@themeisle.com>
2016-02-05 19:49:00 +0200

    !!! added imagga plugin

---
+++ includes/class-imagga-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.themeisle.com
+ * @since      1.0.0
+ *
+ * @package    Imagga
+ * @subpackage Imagga/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Imagga
+ * @subpackage Imagga/includes
+ * @author     Themeisle <friends@themeisle.com>
+ */
+class Imagga_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
